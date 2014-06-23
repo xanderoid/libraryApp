5b707542dea4e7fa7fb8d313c80c3e75de41f74c
Xander Taylor <xanderoid@gmail.com>
2014-06-23 09:57:51 -0700

    added index&entry.html and associated css from bootstrap examples (login, sticky-footer-nav)

---
+++ js/library.js
@@ -0,0 +1,6 @@
+//scan barcode loads links to amazon website (or other info source)
+//writes book info to object/database
+	//title
+	//author
+	//publication database
+//shows book as available in library
